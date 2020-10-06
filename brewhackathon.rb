71bc8c8d20e0249f5e666653206f0b27a5845a46
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-06 13:07:20 +0100

    add formula

---
+++ brewhackathon.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Brewhackathon < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pritamwipro/brewHackathon/archive/v1.0.0.tar.gz"
+  sha256 "36e022f37ece3cdafaa5edbcc0a8a6217e63985c1a5eb607ad5346c2f960a0e5"
+  license ""
+
+  def install
+    bin.install "brewHackathon"
+  end
+
+end
